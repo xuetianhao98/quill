9f35dae43678e4bf48b4312d8472d1c0e9a75e0a
theo <xuethlovelicy@gmail.com>
2026-04-19 21:37:35 +0800

    Reorganize #ifdef guards and private members in BoundedSPSCQueueImpl

--- include/quill/core/BoundedSPSCQueue.h
+++ include/quill/core/BoundedSPSCQueue.h
@@ -326,7 +326,11 @@ private:
   }
 
 private:
+#ifdef QUILL_X86ARCH
   static constexpr integer_type QUILL_CACHE_LINE_MASK{QUILL_CACHE_LINE_SIZE - 1};
+  integer_type _last_flushed_writer_pos{0};
+  integer_type _last_flushed_reader_pos{0};
+#endif
 
   integer_type const _capacity;
   integer_type const _mask;
@@ -336,13 +340,11 @@ private:
 
   alignas(QUILL_CACHE_LINE_ALIGNED) std::atomic<integer_type> _atomic_writer_pos{0};
   alignas(QUILL_CACHE_LINE_ALIGNED) integer_type _writer_pos{0};
-  integer_type _reader_pos_cache{0};
-  integer_type _last_flushed_writer_pos{0};
+  mutable integer_type _writer_pos_cache{0};
 
   alignas(QUILL_CACHE_LINE_ALIGNED) std::atomic<integer_type> _atomic_reader_pos{0};
   alignas(QUILL_CACHE_LINE_ALIGNED) integer_type _reader_pos{0};
-  mutable integer_type _writer_pos_cache{0};
-  integer_type _last_flushed_reader_pos{0};
+  integer_type _reader_pos_cache{0};
 };
 
 using BoundedSPSCQueue = BoundedSPSCQueueImpl<size_t>;
